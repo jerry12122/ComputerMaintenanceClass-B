b10df9bd6fa5650c05e18001fc3ed96439694547
jerry12122 <a0975844268@gmail.com>
2018-12-17 14:44:54 +0800

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.ovalShape16 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape15 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape14 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape13 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape12 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape11 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape10 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape9 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape8 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape7 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape6 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape5 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape4 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape3 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape2 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.ovalShape16,
+            this.ovalShape15,
+            this.ovalShape14,
+            this.ovalShape13,
+            this.ovalShape12,
+            this.ovalShape11,
+            this.ovalShape10,
+            this.ovalShape9,
+            this.ovalShape8,
+            this.ovalShape7,
+            this.ovalShape6,
+            this.ovalShape5,
+            this.ovalShape4,
+            this.ovalShape3,
+            this.ovalShape2,
+            this.ovalShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(580, 356);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // ovalShape16
+            // 
+            this.ovalShape16.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape16.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape16.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape16.Location = new System.Drawing.Point(530, 129);
+            this.ovalShape16.Name = "ovalShape16";
+            this.ovalShape16.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape15
+            // 
+            this.ovalShape15.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape15.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape15.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape15.Location = new System.Drawing.Point(497, 129);
+            this.ovalShape15.Name = "ovalShape15";
+            this.ovalShape15.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape14
+            // 
+            this.ovalShape14.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape14.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape14.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape14.Location = new System.Drawing.Point(464, 129);
+            this.ovalShape14.Name = "ovalShape14";
+            this.ovalShape14.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape13
+            // 
+            this.ovalShape13.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape13.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape13.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape13.Location = new System.Drawing.Point(431, 129);
+            this.ovalShape13.Name = "ovalShape13";
+            this.ovalShape13.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape12
+            // 
+            this.ovalShape12.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape12.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape12.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape12.Location = new System.Drawing.Point(398, 129);
+            this.ovalShape12.Name = "ovalShape12";
+            this.ovalShape12.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape11
+            // 
+            this.ovalShape11.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape11.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape11.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape11.Location = new System.Drawing.Point(365, 129);
+            this.ovalShape11.Name = "ovalShape11";
+            this.ovalShape11.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape10
+            // 
+            this.ovalShape10.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape10.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape10.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape10.Location = new System.Drawing.Point(332, 129);
+            this.ovalShape10.Name = "ovalShape10";
+            this.ovalShape10.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape9
+            // 
+            this.ovalShape9.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape9.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape9.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape9.Location = new System.Drawing.Point(299, 129);
+            this.ovalShape9.Name = "ovalShape9";
+            this.ovalShape9.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape8
+            // 
+            this.ovalShape8.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape8.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape8.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape8.Location = new System.Drawing.Point(266, 129);
+            this.ovalShape8.Name = "ovalShape8";
+            this.ovalShape8.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape7
+            // 
+            this.ovalShape7.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape7.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape7.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape7.Location = new System.Drawing.Point(233, 129);
+            this.ovalShape7.Name = "ovalShape7";
+            this.ovalShape7.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape6
+            // 
+            this.ovalShape6.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape6.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape6.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape6.Location = new System.Drawing.Point(200, 129);
+            this.ovalShape6.Name = "ovalShape6";
+            this.ovalShape6.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape5
+            // 
+            this.ovalShape5.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape5.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape5.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape5.Location = new System.Drawing.Point(167, 129);
+            this.ovalShape5.Name = "ovalShape5";
+            this.ovalShape5.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape4
+            // 
+            this.ovalShape4.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape4.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape4.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape4.Location = new System.Drawing.Point(134, 129);
+            this.ovalShape4.Name = "ovalShape4";
+            this.ovalShape4.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape3
+            // 
+            this.ovalShape3.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape3.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape3.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape3.Location = new System.Drawing.Point(101, 129);
+            this.ovalShape3.Name = "ovalShape3";
+            this.ovalShape3.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape2
+            // 
+            this.ovalShape2.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape2.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape2.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape2.Location = new System.Drawing.Point(68, 129);
+            this.ovalShape2.Name = "ovalShape2";
+            this.ovalShape2.Size = new System.Drawing.Size(25, 23);
+            // 
+            // ovalShape1
+            // 
+            this.ovalShape1.BackColor = System.Drawing.SystemColors.Control;
+            this.ovalShape1.FillColor = System.Drawing.SystemColors.Control;
+            this.ovalShape1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape1.Location = new System.Drawing.Point(33, 129);
+            this.ovalShape1.Name = "ovalShape1";
+            this.ovalShape1.Size = new System.Drawing.Size(25, 23);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(545, 102);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Green LED";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(399, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Red LED";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(211, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 54);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "EXIT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 356);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "電腦硬體裝修乙級第一站第1題";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape16;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape15;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape14;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape13;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape12;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape11;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape10;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape9;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape8;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape7;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape6;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape5;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape4;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape3;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape2;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.PowerPacks;
+using LibUsbDotNet.Main;
+using LibUsbDotNet;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        byte[] green = new byte[8] { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80 };
+        int btn, cnt;
+        UsbDevice dev;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //---------------------------------------------------------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btn = 1;
+            cnt = 0;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            btn = 2;
+            cnt = 0;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            btn = 3;
+        }
+        //---------------------------------------------------------------------------------
+        private OvalShape G(int i)
+        {
+            OvalShape[] a = new OvalShape[] { ovalShape1, ovalShape2, ovalShape3, ovalShape4, ovalShape5, ovalShape6, ovalShape7, ovalShape8 };
+            return a[7-i];
+        }
+        private OvalShape R(int i)
+        {
+            OvalShape[] a = new OvalShape[] { ovalShape9, ovalShape10, ovalShape11, ovalShape12, ovalShape13, ovalShape14, ovalShape15, ovalShape16 };
+            return a[7-i];
+        }
+        //---------------------------------------------------------------------------------
+        public void ShowG(int n)
+        {
+
+            for (int i = 0; i < 8; i++)
+            {
+                G(i).BackStyle = BackStyle.Opaque;
+                if (n % 2 == 1)
+                {
+                    G(i).FillColor = Color.FromArgb(255, 0, 255, 0);
+                }
+                else
+                {
+                    G(i).FillColor = Color.FromArgb(255, 0, 128, 0);
+                }
+                n = n / 2;
+            }
+        }
+        public void ShowR(int n)
+        {
+
+            for (int i = 0; i < 8; i++)
+            {
+                R(i).BackStyle = BackStyle.Opaque;
+                if (n % 2 == 1)
+                {
+                    R(i).FillColor = Color.FromArgb(255, 255, 0, 0);
+                }
+                else
+                {
+                    R(i).FillColor = Color.FromArgb(255, 128, 0, 0);
+                }
+                n = n / 2;
+            }
+        }
+        //---------------------------------------------------------------------------------
+
+        private static void Usbout(UsbDevice dev, byte data, byte ctrl)
+        {
+
+            int transferred = 0;
+            byte[] dataBuffer = new byte[2] { data, ctrl };
+            UsbSetupPacket UsbSetup = new UsbSetupPacket(32, 9, 0, 0, 0);
+            dev.ControlTransfer(ref UsbSetup, dataBuffer, 8 , out transferred);
+
+        }
+        //---------------------------------------------------------------------------------
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "Current Time:" + DateTime.Now.ToString("HH:mm:ss");
+            dev = UsbDevice.OpenUsbDevice(new UsbDeviceFinder(0x1234, 0x6789));
+
+            
+            if (dev == null)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    R(i).BackStyle = BackStyle.Transparent;
+                    G(i).BackStyle = BackStyle.Transparent;
+                }
+                if (btn == 3)
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                ShowG(0);
+                ShowR(0);
+                Usbout(dev, 0, 0);
+                Usbout(dev, 0, 0x10);
+                if (btn == 1 && cnt < green.Length)
+                {
+                    ShowG(green[cnt]);
+                    Usbout(dev, green[cnt], 0);
+                    cnt++;
+                }
+                else if (btn == 2 && cnt < 8)
+                {
+                    int n = (int)Math.Pow(2, cnt);
+                    ShowR(n);
+                    Usbout(dev, (byte)n, 0x20);
+                    Usbout(dev, (byte)n, 0x30);
+                    cnt++;
+                }
+                else if (btn == 3)
+                {
+                    dev.Close();
+                    this.Close();
+                }
+
+            }
+        }
+    }
+}
